34b0312257a50795c8bb233fe0659478bad2454c
Molly Felts Vallin <mnfelts@gmail.com>
2023-04-21 18:53:59 +0200

    application.tsx
    - created placeholder file for app routes

---
+++ server/src/config/application.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export interface IApplicationProps {}
+
+const Application: React.FunctionComponent<IApplicationProps> = (props) => {
+    // TO-DO add routing
+    return null;
+};
+
+export default Application;
